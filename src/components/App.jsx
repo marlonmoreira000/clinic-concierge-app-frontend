98e0f3ffb25f8086be40bfd1a3b6b6a20c68ed35
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-07-11 14:22:12 +0800

    create basic page layout with react routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Nav from "./Nav";
 import Home from "./pages/Home"
+import About from "./pages/About";
+import Doctors from "./pages/Doctors";
+import Bookings from "./pages/Bookings";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Nav/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
