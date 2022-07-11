7eb2b7328ab8663d9bf773bb4cc7802a4be79238
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-07-11 14:04:52 +0800

    set up react router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+
 const App = () => {
   return (
-    <h1>Home</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
