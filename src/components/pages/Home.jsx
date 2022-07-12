6f2d1a321e8b488a0079fa82573bab281e84e613
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-07-12 13:51:33 +0800

    add tailwind css

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,16 @@
 const Home = () => {
     return (
-        <>
-            <hr />
-            <h1>Home big banner heading</h1>
-            <hr />
-            <h1>Content</h1>
-            <hr />
-        </>
-    )
+      <>
+        <hr />
+        <h1>Home big banner heading</h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
+        <hr />
+        <h1>Content</h1>
+        <hr />
+      </>
+    );
 }
 
 export default Home
