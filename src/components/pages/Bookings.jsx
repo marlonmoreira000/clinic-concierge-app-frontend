bf76c0df1a76f302a91198578c1ff7596da3d39c
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-07-16 11:14:59 +0800

    add dummy doctor data and dynamic timeslots

--- src/components/pages/Bookings.jsx
+++ src/components/pages/Bookings.jsx
@@ -1,73 +1,81 @@
 import Appointments from "../Appointments";
-import { DatePicker, Button, Dropdown, Menu, Col, Row } from "antd";
-import React from "react";
-
+import { DatePicker, Button, Dropdown, Menu, Space } from "antd";
+import React, { useState } from "react";
 
 const Bookings = () => {
-  const onChange = (date, dateString) => {
-    console.log(date, dateString);
-  };
+  // dynamic data ("state")
+  const [doctor, setDoctor] = useState(false);
+
+  // static data
+  const doctors = {
+    doctorA: {
+      appointments: [1, 2, 3, 4, 5],
+    },
+    doctorB: {
+      appointments: [1, 2, 4, 5],
+    },
+    doctorC: {
+      appointments: [2, 3, 4],
+    },
+  }
 
   const menu = (
     <Menu
+      onClick={handleMenuClick}
       items={[
         {
+          label: "doctorA",
           key: "1",
-          label: (
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.antgroup.com"
-            >
-              1st menu item
-            </a>
-          ),
         },
         {
+          label: "doctorB",
           key: "2",
-          label: (
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.aliyun.com"
-            >
-              2nd menu item
-            </a>
-          ),
         },
         {
+          label: "doctorC",
           key: "3",
-          label: (
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.luohanacademy.com"
-            >
-              3rd menu item
-            </a>
-          ),
         },
       ]}
     />
   );
 
-    return (
-      <div class="w-full p-8">
-        <div class="text-center">
-          {/* filter bar */}
-          <div>
-            <DatePicker onChange={onChange} />
-            <Dropdown overlay={menu} placement="bottom">
-              <Button class="">bottom</Button>
-            </Dropdown>
-          </div>
-          {/* time slots */}
-          <div>
-            
-          </div>
+  // functions
+  const onChange = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
+  const handleMenuClick = (e) => {
+    const doctorSelection = e.domEvent.target.textContent;
+    console.log(doctorSelection);
+    setDoctor(doctorSelection);
+  };
+
+  // return statement
+  return (
+    <div className="w-full p-8">
+      <div className="text-center">
+        {/* filter bar */}
+        <div>
+          <DatePicker onChange={onChange} />
+          <Dropdown overlay={menu} placement="bottom">
+            <Button>Choose Doctor</Button>
+          </Dropdown>
+        </div>
+        {/* time slots */}
+        <div>
+          <ul>
+            <li>
+              {doctor
+                ? doctors[doctor].appointments.map((item, index) => {
+                    return <li key={index}>{item}</li>;
+                  })
+                : "select a doctor"}
+            </li>
+          </ul>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Bookings
+export default Bookings;
