2f58cf7f43efd6abaf858d73c637ac32a9370c18
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-07-19 10:05:09 +0800

    rename class to classNAme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = (props) => {
   return (
     <div className="bg-[#23375d]">
-      <h1 class="text-center text-4xl sm:text-6xl md:text-7xl font-bold py-16 md:py-22 lg:py-28 text-[#f0edeb]">
+      <h1 className="text-center text-4xl sm:text-6xl md:text-7xl font-bold py-16 md:py-22 lg:py-28 text-[#f0edeb]">
         {props.text}
       </h1>
     </div>
