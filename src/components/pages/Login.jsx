4c3d94f8825829e3f28acb68a1942c319000fcea
Champ-Dog <champdog.code@gmail.com>
2022-07-19 10:41:09 +1000

    Registration functional, login broken

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
-import FormInput from "../form_components/FormInput";
-import { Link } from "react-router-dom";
+import { Form, Input, Button } from "antd";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useToken } from "../auth/useToken";
 import toast from "react-hot-toast";
 
 const Login = () => {
-  const [token, setToken] = useToken ()
-  
+  const navigate = useNavigate();
+  const [token, setToken] = useToken();
+
   // Login
   const onFinish = async (values) => {
     try {
-      const response = await axios.post("/api/user/login", values);
+      const response = await axios.post("/api/v1/login", values);
       if (!response.data.error) {
         toast.success(response.data.message);
-        localStorage.setItem("token", response.data.data);
+        console.log(response.data)
         // navigate("/");
       } else {
         toast.error(response.data.message);
@@ -24,58 +24,45 @@ const Login = () => {
     }
   };
 
-  //Form Data
-
-  // Use state to update form input fields with user-supplied values - setting initial values to empty
-  const [values, setValues] = useState({
-    email: "",
-    password: "",
-  });
-
-  // Array of input/form fields to render
-  const inputs = [
-    {
-      id: 1,
-      name: "email",
-      type: "email",
-      placeholder: "Email",
-      errorMessage: "Must be a valid email address",
-      label: "Email",
-      required: true,
-    },
-    {
-      id: 2,
-      name: "password",
-      type: "password",
-      placeholder: "Password",
-      label: "Password",
-      required: true,
-    },
-  ];
-
-  // Update input fields with user-supplied values
-  const onChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
   return (
-    <div className="loginForm">
-      <form onFinish={onFinish}>
-        <h1>Register</h1>
+    <div className="authentication">
+      <div className="login">
+        <h1 className="card-title">Sign In</h1>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Email is required",
+              },
+            ]}
+          >
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              ,
+              {
+                required: true,
+                message: "Password is required",
+              },
+            ]}
+          >
+            <Input placeholder="Password" type="password" />
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Sign In!
+          </Button>
 
-        {/* Render form/input field for each input supplied above */}
-        {inputs.map((input) => (
-          <FormInput
-            key={input.id}
-            {...input}
-            value={values[input.name]}
-            onChange={onChange}
-          />
-        ))}
-        <button>Log In</button>
-      </form>
-      <Link to="/register">Register</Link>
-      <Link to="/forgot-password">Forgotten Password?</Link>
+          <Link className="anchor" to="/register">
+            Register
+          </Link>
+        </Form>
+      </div>
     </div>
   );
 };
